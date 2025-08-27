a68d92afdf0df743295514b1325d5ec78fe5ab2d
vitaFlash <vitaliaflash@gmail.com>
2025-08-27 11:14:17 +0200

    feat(repository): add JPA repositories for users, tickets, comments and worklogs

--- src/main/java/com/hive/help/repository/CommentRepository.java
+++ src/main/java/com/hive/help/repository/CommentRepository.java
@@ -1,4 +1,9 @@
 package com.hive.help.repository;
 
-public interface CommentRepository {
+import com.hive.help.model.Comment;
+import org.springframework.data.domain.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Page<Comment> findByTicketId(Long ticketId, Pageable pageable);
 }
